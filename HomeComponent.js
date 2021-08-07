5a1053a41a7425cb90c1c68d859528288afad85d
richardikang <76178368+richardikang@users.noreply.github.com>
2021-08-07 10:42:01 +0200

    Add files via upload

--- HomeComponent.js
+++ HomeComponent.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 function Home() {
-    return (
-        fetch('foundation.json')
-        .then(response => response.json())
-        .then(data => console.log(data))
-    );
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState
+  (true);
+  const [error, setError] = useState
+  (null);
+
+  useEffect(() => {
+    axios('https://foundation.app/api')
+    .then(response => {
+      return response.json()
+    })
+    throw response;
+  })
+    .then(data => {
+      setData(data);
+    })
+    .catch(error => {
+      console.error("Error fetching data: ", error");
+      setError(error);
+    })
+    .finally(() => {
+      setLoading(false);
+    })
 }
+
+if (loading) return "Loading...";
+if (error) return "Error!";
+
     
 export default Home; 
