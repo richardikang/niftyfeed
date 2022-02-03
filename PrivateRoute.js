96eaa8bd8b386cbea36906b8d5ec74abda6abe55
richardikang <76178368+richardikang@users.noreply.github.com>
2022-02-03 09:57:29 +0100

    Add files via upload

--- PrivateRoute.js
+++ PrivateRoute.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
- const PrivateRoute = ({component: Component, rest}) => {
+ const PrivateRoute = ({component: Component, ...rest}) => {
 
     const { currentUser } =useAuth()
 
     return (
         <Route>
-            {rest}
+            {...rest}
             render={props => {
             return currentUser ? <Component {...props} /> : <Redirect to="/login" />
             }}
