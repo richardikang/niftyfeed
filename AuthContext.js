67b786bbaee078a2f16ccbde6752fe70d44713b9
richardikang <76178368+richardikang@users.noreply.github.com>
2022-02-08 22:46:49 +0100

    Add files via upload

--- AuthContext.js
+++ AuthContext.js
@@ -23,6 +23,10 @@ export function AuthProvider({ children }) {
         return auth.signOut()
     }
 
+    function resetPassword(username, email) {
+        return auth.sendPasswordResetEmail()
+    }
+
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user)
@@ -36,7 +40,8 @@ export function AuthProvider({ children }) {
         currentUser,
         login,
         signup,
-        logout
+        logout,
+        resetPassword
     }
 
     return (
