21f0c9ea4a393bef3087c25e011351b556813845
richardikang <76178368+richardikang@users.noreply.github.com>
2022-02-24 16:03:01 +0100

    Add files via upload

---
+++ NFTContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import NFTCard from './NFTCard'
+
+const NFTContainer = ({ nfts }) => {
+
+    return (
+        <div>
+           {nfts.map((nft, index) => {
+               return <NFTCard nft={nft} key={index} />
+           })} 
+        </div>
+    )
+}
+
+export default NFTContainer
