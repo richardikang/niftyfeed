89d756e579c11d39803ae43789bab9de7f0e490b
richardikang <76178368+richardikang@users.noreply.github.com>
2022-08-02 12:47:26 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,42 +1,53 @@
 import React from "react";
-// import { Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import { AuthProvider } from "./contexts/AuthContext";
+import { AuthProvider } from "./contexts/AuthContext";
+import { MoralisProvider } from "react-moralis";
 
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Home from "./pages/Home";
-// import Demo from "./pages/Demo";
-// import Account from "./pages/Accountwizard";
-// import Registration from "./pages/Registration";
-// import Dashboard from "./pages/Dashboard";
-// import PrivateRoute from "./components/PrivateRoute";
-// import ForgotPassword from "./pages/ForgotPassword";
+import Demo from "./pages/Demo";
+import Account from "./pages/Accountwizard";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+import Dashboard from "./pages/Dashboard";
+import ForgotPassword from "./pages/ForgotPassword";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-// import Dashboard from './pages/Dashboard';
 import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
-    // <AuthProvider>
-    //   <Router>
-    //     <>
-    //       <Routes>
-    //         <Route path="/registration" component={Registration} />
-    //         <PrivateRoute exact path="/dashboard" component={Dashboard} />
-    //         <Route exact path="/forgotpassword" component={ForgotPassword} />
-    //         <Route exact path="/about" component={About} />
-    //         <Route exact path="/contact" component={Contact} />
-    //         <Route exact path="/demo" component={Demo} />
-    //         <Route exact path="/account" component={Account} />
-    //         <Route path="/" component={Home} />
-    //       </Routes>
-    //     </>
-    //   </Router>
-    // </AuthProvider>
+    <AuthProvider>
+      <MoralisProvider
+        serverUrl="https://bke4qvnxg7uc.usemoralis.com:2053/server"
+        appId="njkYZk54FWda03ZyCOu4wg4BxbYHB5FaMQ1Nuobu"
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/registration" element={<Registration />} />
+            <Route path="/login" element={<Login />} />
+            <Route exact path="/forgotpassword" element={<ForgotPassword />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/contact" element={<Contact />} />
+            <Route exact path="/demo" element={<Demo />} />
+            <Route exact path="/account" element={<Account />} />
+            <Route path="/redirect" element={<Navigate to="/registration" />} />
+            <Route path="/redirect" element={<Navigate to="/demo" />} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </MoralisProvider>
+    </AuthProvider>
   );
 };
 
